af7eac52014e3ac35a61f9373830444a14414e56
Комзарев Илья Андреевич <komzarev.i@spectronxray.ru>
2023-05-12 15:02:18 +0300

    fix: ipv6 bug

--- app.py
+++ app.py
@@ -7,49 +7,45 @@ app = Flask(__name__)
 @app.route('/avahi', methods=['GET', 'POST'])
 def avahi():
     if request.method == 'POST':
-        id_ = None
+        host = None
         new_name = None
         for key in request.form:
             if key.startswith('new_name.'):
-                tmp = request.form[key]
-                if tmp:
-                    new_name = request.form[key]
-                    id_ = key.partition('.')[-1]
+                new_name = request.form[key]
+                print(f"Before New name: {key} host: {new_name}")
+                if new_name:
+                    host = key.partition('.')[-1]
+                    break
 
-        if id_  and new_name:
-            print(f"New name: {new_name} row: {id_}")
+        if host  and new_name:
+            print(f"New name: {new_name} host: {host}")
             cmds = f"\"hostnamectl set-hostname {new_name} && systemctl restart avahi-daemon\""
-            host = id_
             subprocess.Popen(f"ssh -oStrictHostKeyChecking=no root@{host} {cmds}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
             sleep(5)  
 
       
     data = []
-    cmd = "avahi-browse -a -t | grep SSH"
+    cmd = "avahi-browse --resolve -t -p _ssh._tcp | grep =;"
     output = subprocess.check_output(cmd, shell=True).decode()
     raw = output.split("\n")
     for i in raw:
         d = []
-        d.append("SSH")
-        spl = i.split(" ")
-        if len(spl) < 4:
+        spl = i.split(";")
+        if len(spl) < 8:
             continue
+
         name = spl[3] + ".local"
         ipv = spl[2]
+
+        if ipv != "IPv4":
+            continue
+
+        d.append("SSH")
         d.append(ipv)
         d.append(name)
-        if ipv == "IPv4":
-            resolveCmd = "avahi-resolve-host-name -4 " + name
-        else:
-            resolveCmd = "avahi-resolve-host-name -6 " + name
-
-        output = subprocess.check_output(resolveCmd, shell=True).decode()
-        out2 = output.split("\t")
-        if len(out2) > 1:
-            d.append(out2[1])
-        else:
-            d.append("<ERROR>")
-        d.append("22")
+        d.append(spl[7]) #ip
+        port = spl[8]
+        d.append(port)
         data.append(d)
     return render_template("avahi.html", rows=data)
 
