0b24aed355dbc7ca8004b4378aea11a5c55ffb35
Ilya Komzarev <komzarev.i@spectronxray.ru>
2024-12-26 18:34:42 +0300

    API возвращает еэш

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 
+import asyncio
 import json
-from fastapi import FastAPI, Request,Form
+from fastapi import FastAPI, Request,Form,BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import search_devices
 from typing import Annotated
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(update_chache())
+    yield
+    
+app = FastAPI(lifespan=lifespan)
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+global_devices = []
+
+async def update_chache():
+    global global_devices
+    global_devices = search_devices.get_services()
+    await asyncio.sleep(10)
+
 @app.get("/")
-def index(request: Request):
-    merged_services = search_devices.get_services()
-    return templates.TemplateResponse("avahi.html", {"request": request, "rows" : merged_services, "comments" : search_devices.comments})
+async def index(request: Request):
+    global global_devices
+    global_devices = search_devices.get_services()
+    return templates.TemplateResponse("avahi.html", {"request": request, "rows" : global_devices, "comments" : search_devices.comments})
 
 @app.get('/api')
-def avahi_api(request: Request):
-    merged_services = search_devices.get_services()
+async def avahi_api(request: Request):
+    global global_devices
+    merged_services = global_devices
     all = {}
     for service in merged_services:
         ret = {}
@@ -36,7 +52,7 @@ class CommentUpdate(BaseModel):
     comment: str
     
 @app.post('/set-comment')
-def set_comment(data: CommentUpdate, request: Request):
+async def set_comment(data: CommentUpdate, request: Request):
     host = data.dev_name.partition('.')[-1]
     if host:
         search_devices.comments[host] = data.comment
@@ -44,4 +60,3 @@ def set_comment(data: CommentUpdate, request: Request):
     with open(search_devices.fileName, 'w') as f:
         json.dump(search_devices.comments, f)
     return f"for {data.dev_name} set comment {data.comment}"
-
