bdde2e79966effe648b24d9d0b9ffe1fc88c074a
komzarev <ilya.komzarev@gmail.com>
2025-02-10 14:42:10 +0300

    Add logging

--- services_repository.py
+++ services_repository.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import json
 import os
 from typing import Dict, List
@@ -5,7 +6,8 @@ from pydantic import BaseModel
 from zeroconf import ServiceBrowser, ServiceInfo, ServiceListener, Zeroconf, IPVersion
 import socket
 import aioping
-
+import logging
+logger = logging.getLogger(__name__)
 
 class MyServiceInfo(BaseModel):
     ping: str = None
@@ -14,6 +16,12 @@ class MyServiceInfo(BaseModel):
     name: str
     ip: str
     comment: str
+
+PING_UNTIL_REMOVE = 3
+@dataclass
+class MyServiceRecord:
+    ping_left: int = PING_UNTIL_REMOVE
+    data: MyServiceInfo = None
 
 class MyListener(ServiceListener):
     default = "None"
@@ -24,7 +32,7 @@ class MyListener(ServiceListener):
         self.processors["_spectron._tcp.local."] = self._info_by_type_spectron
         self.processors["_ssh._tcp.local."] = self._info_by_type_ssh
 
-    def _info_by_type_spectron(self, si:ServiceInfo) -> MyServiceInfo:
+    def _info_by_type_spectron(self, si:ServiceInfo) -> MyServiceRecord:
         msi = MyServiceInfo(
             name = si.server,
             ip = self._get_ip(si.addresses),
@@ -32,7 +40,7 @@ class MyListener(ServiceListener):
             os_version = si.properties[b'version'],
             comment = "" ,
         )
-        return msi
+        return MyServiceRecord(PING_UNTIL_REMOVE,msi)
     
     def _get_ip(self,addresses: List[bytes]) -> str:
         try:
@@ -41,7 +49,7 @@ class MyListener(ServiceListener):
             print(ex)
         return MyListener.default
     
-    def _info_by_type_ssh(self, si:ServiceInfo) -> MyServiceInfo:
+    def _info_by_type_ssh(self, si:ServiceInfo) -> MyServiceRecord:
         msi = MyServiceInfo(
             name = si.server,
             ip = self._get_ip(si.addresses),
@@ -49,50 +57,60 @@ class MyListener(ServiceListener):
             os_version = MyListener.default,
             comment = "" ,
         )
-        return msi
+        return MyServiceRecord(PING_UNTIL_REMOVE,msi)
     
     def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
         info = zc.get_service_info(type_, name)
         self.services[info.server]  = self.processors[type_](info)
-        print(f"Service {name} updated")
+        logger.warning(f"ZeroConf Service {name} updated")
 
     def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
         info = zc.get_service_info(type_, name)
-        self.services.pop(info.server)
-        print(f"Service {info.server} removed")
+        if info.server in self.services:
+            self.services.pop(info.server)
+        logger.warning(f"ZeroConf Service {info.server} removed")
 
     def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
         info = zc.get_service_info(type_, name)
         if not info:
-            print(f"not info {name}")
+            logger.warning(f"no info  for {name}")
             return
         self.services[info.server] = self.processors[type_](info)
         
-        print(f"Service {info.server} added, service info: {info}")
+        logger.warning(f"ZeroConf Service {info.server} added, service info: {info}")
 
 class ServiceRepository:
     def __init__(self):
-        self.services: Dict[str,MyServiceInfo] = {}
+        self.services: Dict[str,MyServiceRecord] = {}
         self.zeroconf = Zeroconf(ip_version=IPVersion.V4Only)
         self.listener = MyListener(self.services)
         self.browser = ServiceBrowser(self.zeroconf, ["_spectron._tcp.local.","_ssh._tcp.local."], self.listener,delay=1)
         self.comment_fileName = "avahi-comments.json"
         self.comments = {}
+        logger.critical("===========Started===========")
         if os.path.exists(self.comment_fileName):
             with open(self.comment_fileName, 'r') as f:
                 self.comments = json.load(f)
         
     async def get_services(self) -> List[MyServiceInfo]:
         all: List[MyServiceInfo] = []
+        to_remove = []
         for k,i in self.services.items():
-            i.comment = self.comments[k] if k in self.comments else ""
+            i.data.comment = self.comments[k] if k in self.comments else ""
             try:
-                png = await aioping.ping(i.ip,timeout=3) * 1000 #"ms"
+                png = await aioping.ping(i.data.ip,timeout=3) * 1000 #"ms"
+                i.data.ping = f"{png:.1f} ms"
+                all.append(i.data)
+                i.ping_left = PING_UNTIL_REMOVE
+                logger.info(f"Ping {k} - {i.data.ping}")
             except:
-                png = None
-            if png:
-                i.ping = f"{png:.1f} ms"
-                all.append(i)
+                i.ping_left -= 1
+                logger.info(f"No ping for {k}, left {i.ping_left}")
+                if i.ping_left <= 0:
+                    to_remove.append(k)
+                    logger.warning(f"Removed {k}")
+        for k in to_remove:
+            self.services.pop(k)
         return all
     
     def set_service_comment(self, name, comment):
